0768a97264ad230a673dc0fea681cb9a8d01b071
Mehnaz <mehnaz.gakk@gmail.com>
2023-01-17 15:58:37 +0600

    billboard fix

--- app/src/main/java/com/co/shadhinmusicsdk/FeaturedHomeFragment.kt
+++ app/src/main/java/com/co/shadhinmusicsdk/FeaturedHomeFragment.kt
@@ -73,7 +73,7 @@ internal class FeaturedHomeFragment : Fragment() {
         }
 
         btnMusic.setOnClickListener {
-            ShadhinMusicSdkCore.openShadhin(requireContext())
+            ShadhinMusicSdkCore.openShadhin(requireContext(), "01234556666666")
         }
         var count:Int = 0
         btnShare.setOnClickListener {
